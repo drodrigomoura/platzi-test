88e6271b61ea59c1c7067dff503be77dd61e6619
Rodrigo Moura <d.rodrigomoura@gmail.com>
2021-07-21 23:01:26 -0300

    Arrays

---
+++ __test__/arrays.test.js
@@ -0,0 +1,15 @@
+import { arrayColors, arrayFruits } from "../arrays";
+
+describe("Exits an element", () => {
+  it("should contains a banana ", () => {
+    expect(arrayFruits()).toContain("banana");
+  });
+
+  it("should not contains a platano ", () => {
+    expect(arrayFruits()).not.toContain("platano");
+  });
+
+  it("should have a lenght === 6", () => {
+    expect(arrayFruits()).toHaveLength(6);
+  });
+});
