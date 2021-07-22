2f495e2db0ecca2fa749126cb340fa5a152a7df7
Rodrigo Moura <d.rodrigomoura@gmail.com>
2021-07-22 18:23:04 -0300

    promise: reject and resolve

--- __test__/promise.test.js
+++ __test__/promise.test.js
@@ -2,13 +2,21 @@ import { getDataFromApi } from "../promise";
 
 
 describe('Test promises', () => {
-  it('get data from API', (done) => {
+  it('get data from API', done => {
     const api = 'https://rickandmortyapi.com/api/character/'
 
     getDataFromApi(api)
       .then(data => {
-        expectExport(data.results.length).toBeGreaterThan(0)
+        expect(data.results.length).toBeGreaterThan(0)
         done()
       })
   });
+
+  it("resolve an 'hola!'", () => {
+    return expect(Promise.resolve('hola!')).resolves.toBe('hola!')
+  })
+
+  it("reject with an error", () => {
+    return expect(Promise.reject('Error')).rejects.toBe('Error')
+  })
 })
