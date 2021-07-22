75d3b4e8a2adf4581353e68b59d66616a3b8b70c
Rodrigo Moura <d.rodrigomoura@gmail.com>
2021-07-21 23:08:20 -0300

    Coverage

--- __test__/arrays.test.js
+++ __test__/arrays.test.js
@@ -12,4 +12,8 @@ describe("Exits an element", () => {
   it("should have a lenght === 6", () => {
     expect(arrayFruits()).toHaveLength(6);
   });
+
+  it("should exist a color", () => {
+    expect(arrayColors()).toContain("azul");
+  });
 });
