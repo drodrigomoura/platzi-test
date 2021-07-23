c50359c892dc01e0a6f7793036743a1367d31ad7
Rodrigo Moura <d.rodrigomoura@gmail.com>
2021-07-22 23:03:26 -0300

    Snapshots: excepciones de algunos atributos

--- __test__/snapshot.test.js
+++ __test__/snapshot.test.js
@@ -6,5 +6,24 @@ describe("It's snapshot time", () => {
     expect(getCharacter(rick)).toMatchSnapshot()
   });
 
+  it('Always fails the snapshot', () => {
+    const user = {
+      createAt: new Date(),
+      id: Math.floor(Math.random() * 20)
+    }
+
+    expect(user).toMatchSnapshot()
+  });
+
+  it('Exception for some values', () => {
+    const user = {
+      id: Math.floor(Math.random() * 20),
+      name: "Rick Sanchez"
+    }
+
+    expect(user).toMatchSnapshot({
+      id: expect.any(Number)
+    })
+  });
 
 })
