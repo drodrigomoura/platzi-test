6771d2ca2f927c81a8320f1a33df742783c7b8e4
Rodrigo Moura <d.rodrigomoura@gmail.com>
2021-07-22 22:38:39 -0300

    Async/await - 2

--- __test__/async.test.js
+++ __test__/async.test.js
@@ -1,7 +1,7 @@
 import { getDataFromApi } from "../promise";
 
 describe('Test async/await', () => {
-  it('test to an API', async () => {
+  it('test request to an API', async () => {
     const api = 'https://rickandmortyapi.com/api/character/'
 
     const getRick = 'https://rickandmortyapi.com/api/character/1'
@@ -17,4 +17,24 @@ describe('Test async/await', () => {
       })
   });
 
+  it('Test request to an API with error 404', async () => {
+    const apiError = 'http://httpstat.us/404'
+
+    const peticion = getDataFromApi(apiError)
+
+    await expect(peticion).rejects.toEqual(Error('Request failed with status code 404'))
+  });
+
+  it('Test request to an API with error 500', async () => {
+    const apiError = 'http://httpstat.us/500'
+
+    const peticion = getDataFromApi(apiError)
+
+    await expect(peticion).rejects.toEqual(Error('Request failed with status code 500'))
+  });
+
+  it("Resolve an 'hola'", async () => {
+    await expect(Promise.resolve('hola')).resolves.toBe('hola')
+    await expect(Promise.resolve('Error')).resolves.toBe('Error')
+  });
 })
